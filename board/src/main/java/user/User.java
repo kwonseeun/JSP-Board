b7c4d9c4c647186382ba10105437d7657bf4a8f5
kwonseeun <0115km@naver.com>
2022-06-08 10:41:10 +0900

    UserVO 추가

---
+++ board/src/main/java/user/User.java
@@ -0,0 +1,41 @@
+package user;
+
+public class User {
+	
+	private String userId;
+	private String userPw;
+	private String userName;
+	private String userGender;
+	private String userMail;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserMail() {
+		return userMail;
+	}
+	public void setUserMail(String userMail) {
+		this.userMail = userMail;
+	}
+}
